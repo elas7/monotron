349db13c2e0fe720e960c21982368896502bbd96
Esteban Las <lasesteban@gmail.com>
2019-01-31 19:43:52 +0100

    add github corner

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,28 @@
 // @flow
-import React from 'react';
-import MonotronContainer from '../containers/Monotron';
+import React from "react";
+import MonotronContainer from "../containers/Monotron";
+import GithubCorner from "react-github-corner";
 
 /**
  * App Component.
  */
 export default function App({ audioContext }: { audioContext: AudioContext }) {
   return (
-    <div className="page">
-      <div className="background-gradient" />
-      <div className="page-content">
-        <div className="section section-monotron">
-          <MonotronContainer audioContext={audioContext} />
+    <>
+      <div className="page">
+        <div className="background-gradient" />
+        <div className="page-content">
+          <GithubCorner
+            href="https://github.com/elas7/monotron"
+            bannerColor="#000"
+            octoColor="#fff"
+            svgStyle={{ mixBlendMode: "darken" }}
+          />
+          <div className="section section-monotron">
+            <MonotronContainer audioContext={audioContext} />
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 }
